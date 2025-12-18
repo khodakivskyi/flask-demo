d65c237428f036959879d354944d038152d2560c
khodakivskiy <andriyhodak2007@gmail.com>
2025-12-18 13:07:30 +0200

    integrate sqLite for project

--- run.py
+++ run.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app) # connect db
+
+# Models
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text)
+    release_date = db.Column(db.String(50))
+    cover_image = db.Column(db.String(200))
 
 @app.route('/')
 def index():
@@ -9,4 +26,6 @@ def index():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
